27dc348721424f9c28c54ad420ab7a2417a87c6e
Vulpelo <damian.praski@gmail.com>
2019-01-20 02:14:56 +0100

    New look of Login page

--- views/DefaultController/login.php
+++ views/DefaultController/login.php
@@ -2,10 +2,16 @@
 <html>
 <?php include_once(dirname(__DIR__).'/head.html'); ?>
 
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+<script src="./public/js/DefaultController/login.js"></script>
+
 <body>
 <?php include_once(dirname(__DIR__).'/navBar.html'); ?>
 
-<div class="container">
+<!-- <div class="container">
     <div clas="row">
         <div class="col-sm-6 offset-sm-3">
             <h1 class="panel-header">LOGIN</h1>
@@ -31,8 +37,72 @@
             </form>
         </div>
     </div>
-</div>
+</div> -->
 
+<div class="container">
+    	<div class="row">
+			<div class="col-md-6 col-md-offset-3 loginSetting">
+				<div class=" panel-login">
+					<div class="panel-heading">
+						<div class="row">
+							<div class="col-xs-6">
+								<a href="#" class="active" id="login-form-link">Login</a>
+							</div>
+							<div class="col-xs-6">
+								<a href="#" id="register-form-link">Register</a>
+							</div>
+						</div>
+						<hr>
+					</div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-lg-12">
+
+                                <form id="login-form" action="?page=login" method="post" role="form" style="display: block;">
+									<div class="form-group">
+										<input type="text" name="email" id="username" tabindex="1" class="form-control" placeholder="Email" value="">
+									</div>
+									<div class="form-group">
+										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+									</div>
+
+									<div class="form-group">
+										<div class="row">
+											<div class="col-sm-6 col-sm-offset-3">
+												<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
+											</div>
+										</div>
+									</div>
+                                </form>
+                                
+								<form id="register-form" action="https://phpoll.com/register/process" method="post" role="form" style="display: none;">
+									<div class="form-group">
+										<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+									</div>
+									<div class="form-group">
+										<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
+									</div>
+									<div class="form-group">
+										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+									</div>
+									<div class="form-group">
+										<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
+									</div>
+									<div class="form-group">
+										<div class="row">
+											<div class="col-sm-6 col-sm-offset-3">
+												<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
+											</div>
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
 </body>
 </html> 
