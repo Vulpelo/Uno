e117238bf92c316a456347c51b892b630d176e3f
Vulpelo <damian.praski@gmail.com>
2019-01-25 14:46:25 +0100

    Game: Arrows on screen are showing in which direction queue is going

--- scripts/game/Uno_game/Actors/myHUD.js
+++ scripts/game/Uno_game/Actors/myHUD.js
@@ -1,14 +1,40 @@
 class MyHUD extends Actor {
-    constructor() {
-        super();
+    constructor(pos) {
+        super(pos);
 
-        // let t = new Text(new Vector2d(RenderData.window.clientWidth * 0.75, RenderData.window.clientHeight * 0.8));
-        // t.Text = "Player 0";
-        // this.renderModel = [t];
-        // this.setPosition = new Vector2d(0,0);
+        this.direction = "cw1";
+
+        this.setPosition = new Vector2d(0,0);
+
+        let cw = new MImage(new Vector2d(RenderData.window.clientWidth * 0.5, RenderData.window.clientHeight * 0.5),
+                            Resources.getImagePathPNG(this.direction));
+        cw.setDimensions = [200,200];
+
+        let rM = [cw];
+
+        this.setModel = rM;
+    }
+
+    eventTick() {
+        this.updateDirection();
+    }
+
+    updateDirection() {
+        if (Server.data.board.clockwise == 1) {
+            if (this.direction == 'cw0') {
+                this.direction = 'cw1';
+                this.renderModel[0].Image = Resources.getImagePathPNG(this.direction);
+            }
+        }
+        else {
+            if (this.direction == 'cw1') {
+                this.direction = 'cw0';
+                this.renderModel[0].Image = Resources.getImagePathPNG(this.direction);
+            }
+        }
     }
 
     set Text(text) {
-        // this.renderModel[0].Text = text;
+        this.renderModel[0].Text = text;
     }
 }
